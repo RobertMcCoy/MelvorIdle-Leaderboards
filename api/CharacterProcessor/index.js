0751c3075e1af7185df2d9bbb294473b9dadec06
Rob McCoy <rmccoy6435@gmail.com>
2020-04-16 16:47:24 -0400

    fix: ensure we always let the context object know we are done in the API

--- api/CharacterProcessor/index.js
+++ api/CharacterProcessor/index.js
@@ -3,10 +3,7 @@ const entryProcessor = require('./entryProcessor');
 module.exports = function(context, req) {
     context.log.info(`Request for new entry received by ${req.body.user}`)
     if (req.body.skills && req.body.user) {
-        if (req.body.skills.some(skill => skill > 99)) {
-            return;
-        }
-        else {
+        if (req.body.skills.every(skill => skill <= 120)) {
             entryProcessor.Process(context, req.body.skills, req.body.user, req.body.mastery);
         }
     }
